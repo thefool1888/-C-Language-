667dc9dbcc0bbcbca1995ff87f036d13e924047e
thefool1888 <thefoolwwwww@outlool.com>
2026-05-26 20:44:19 +0800

    practise

---
+++ test_5_26/test_5_26.c
@@ -0,0 +1,57 @@
+#define  _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+
+//int main()
+//{
+//	printf("hallo word\n");
+//	return 0;
+//}
+
+
+//C语言的类型
+//int main()
+//{
+//	printf("%d\n", sizeof(char));		 //字符型
+//	printf("%d\n", sizeof(short));		 //短整形
+//	printf("%d\n", sizeof(int));			 //整形
+//	printf("%d\n", sizeof(long));			//长的整型
+//	printf("%d\n", sizeof(long long));   //更长的整形
+//	printf("%d\n", sizeof(float));			//单精度浮点型
+//	printf("%d\n", sizeof(double));     //双精度浮点型
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d  %d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//
+
+//全局变量和局部变量
+//局部优先
+//int a = 10;
+//int main()
+//{
+//	int a = 2;
+//	printf("%d\n", a);
+//	return 0;
+//
+//}
+
+//变量的作用域
+//int main()
+//{
+//	{
+//		int a = 10;
+//		//a 的作用域为这个括号内
+//	}
+//	printf("%d", a);
+//	return 0;
+//}
