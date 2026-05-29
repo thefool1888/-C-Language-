61683c50ada8e82adca36b15cc573cb473bc8bf0
thefool1888 <thefoolwwwww@outlook.com>
2026-05-29 20:03:36 +0800

    practise

---
+++ test_5_30/test.c
@@ -0,0 +1,65 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int Compare(int x, int y)
+//{
+//	int max = 0;
+//	if (x > y)
+//	{
+//		max = x;
+//	}
+//	else
+//	{
+//		max = y;
+//	}
+//	return max;
+//}
+//
+// int Compare(int x, int y)
+//{
+//	if (x > y)
+//	return  x;
+//	else
+//		return y;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("\n%d", Compare(a, b));
+//	return 0;
+//}
+
+//int main() {
+//    int num = 0;
+//    scanf("%d", &num);
+//    printf("%d", num);
+//    return 0;
+//}
+//
+//int Value(int y)
+//{
+//	if (y < 0)
+//	{
+//		return 1;
+//	}
+//	else if (y == 0)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//int main()
+//{
+//	int x = 0;
+//	scanf("%d", &x);
+//	x =Value(x);
+//	printf("%d", x);
+//	return 0;
+//}
